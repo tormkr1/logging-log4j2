4aa27058f2c7ab255e571b5185393017900a23a5
tormkr1 <141587632+tormkr1@users.noreply.github.com>
2023-12-07 23:39:11 +0100

    Update ProcessIdUtilTest.java
    ghjdghj

--- log4j-api-java9/src/test/java/org/apache/logging/log4j/util/java9/ProcessIdUtilTest.java
+++ log4j-api-java9/src/test/java/org/apache/logging/log4j/util/java9/ProcessIdUtilTest.java
@@ -13,6 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * gjdfgoadfg
  */
 package org.apache.logging.log4j.util.java9;
 
